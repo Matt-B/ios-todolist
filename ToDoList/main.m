edbd2097a6c64db5c8988f2671f1b4cc899a385c
Matt Bailey <matt@Matts-MBP.lan>
2015-07-06 15:46:56 +0100

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Matt Bailey on 06/07/2015.
+//  Copyright (c) 2015 Matt Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
